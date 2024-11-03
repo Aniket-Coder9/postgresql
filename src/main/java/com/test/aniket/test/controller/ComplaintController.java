d26275bc17631555903e062d711cdc2c9a8b4b22
Aniket-Coder9 <infoani998@gmail.com>
2024-11-03 21:18:11 +0530

    Basic Authentication integrated

--- src/main/java/com/test/aniket/test/controller/ComplaintController.java
+++ src/main/java/com/test/aniket/test/controller/ComplaintController.java
@@ -4,6 +4,7 @@ import com.test.aniket.test.service.ComplaintService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,4 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class ComplaintController {
 
     private final ComplaintService service;
+
+//    For Role Base Api Permission
+//    @PreAuthorize("hasAnyAuthority('ROLE_ADMIM')")
 }
